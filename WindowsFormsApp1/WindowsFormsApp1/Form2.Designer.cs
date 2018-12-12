76fc9c23c731bd44287c253de02600743bd89771
minkinone <minkinone@outlook.com>
2018-12-12 09:27:25 +0800

    mk

--- WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(649, 304);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(586, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "确认选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选择你要取消的项目";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(533, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "全选";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(586, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "取消选择";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(673, 424);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form2";
+            this.Text = "取消预约";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -7,14 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class Form2 : Form
     {
+        string Id;
+
+
         public Form2()
         {
             InitializeComponent();
+        }
+
+        SqlConnection cnt = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            cnt.ConnectionString = @"Data Source =.\SQLEXPRESS; Initial Catalog = Hostital1; Integrated Security = True";
+            cmd.Connection = cnt;
+            cmd.CommandText = "select * from [Hostital1].[dbo].[Yc] where ID=@id";
+            cmd.Parameters.AddWithValue("@id", Id);
+            da.SelectCommand = cmd;
+            try
+            {
+                cnt.Open();
+                da.Fill(dt);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                da.Dispose();
+                cmd.Dispose();
+                cnt.Close();
+            }
+            listBox1.BeginUpdate();
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                listBox1.Items.Add(dt.Rows[i][0].ToString()+ dt.Rows[i][1].ToString() + dt.Rows[i][2].ToString());
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cmd.Connection = cnt;
+            cmd.CommandText = "";
+            if (listBox1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("您没有选择任何项目");
+                cmd.Dispose();
+                return;
+            }
+            for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+            {
+                if (canresign((DateTime)dt.Rows[listBox1.SelectedIndices[i]][2]))
+                {
+                    cmd.CommandText += "delete from [Hostital1].[dbo].[Yc] where ID=@id and Dno=@dno and Ytime=@ytime and flag=@fg ";
+                    cmd.Parameters.AddWithValue("@id", dt.Rows[listBox1.SelectedIndices[i]][0]);
+                    cmd.Parameters.AddWithValue("@dno", dt.Rows[listBox1.SelectedIndices[i]][1]);
+                    cmd.Parameters.AddWithValue("@ytime", dt.Rows[listBox1.SelectedIndices[i]][2]);
+                    cmd.Parameters.AddWithValue("@fg", dt.Rows[listBox1.SelectedIndices[i]][3]);
+                }
+            }
+            try
+            {
+                cnt.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("成功取消");
+                refurbish();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                cmd.Dispose();
+                cnt.Close();
+            }
+        }
+
+        bool canresign(DateTime dt)
+        {
+            DateTime ndt = new System.DateTime();
+            if ((dt-ndt).Days>1)
+                return true;
+            else
+                return false;
+        }
+
+        void refurbish()
+        {
+            listBox1.Items.Clear();
+            cmd.Connection = cnt;
+            cmd.CommandText = "select * from [Hostital1].[dbo].[Yc] where ID=@id";
+            cmd.Parameters.AddWithValue("@id", Id);
+            da.SelectCommand = cmd;
+            try
+            {
+                cnt.Open();
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                da.Dispose();
+                cmd.Dispose();
+                cnt.Close();
+            }
+            listBox1.BeginUpdate();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                listBox1.Items.Add(dt.Rows[i][0].ToString() + dt.Rows[i][1].ToString() + dt.Rows[i][2].ToString());
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    listBox1.SelectedIndex = i;
+                }
+            }
         }
     }
 }
